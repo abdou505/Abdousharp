8108d0e3a411122d9aeb086943fe053a5e4be2b5
abdou505 <54625120+abdou505@users.noreply.github.com>
2019-09-02 17:31:20 +0200

    Add files via upload

---
+++ Abdousharp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abdousharp : MonoBehaviour
+{
+     public int A , B;
+    int C;
+    void start()
+    {
+
+        float C;
+        C = A + B;
+        Debug.Log ("the result is = " + C + " this is a");
+        C = A - B;
+        Debug.Log ("the result is = " + C + " this is b ");
+        C = A * B;
+        Debug.Log ("the result is = " + C + " this is c  ");
+        C = A % B;
+        Debug.Log ("the result is = " + C + " this is  d ");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
+
+
+
+
